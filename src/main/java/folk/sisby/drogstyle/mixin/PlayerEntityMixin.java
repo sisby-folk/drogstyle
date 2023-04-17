cdc50fd535a3b4af82da609ef39b74184c7debb0
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-04-17 17:58:52 +1000

    fix more mixins

--- src/main/java/folk/sisby/drogstyle/mixin/PlayerEntityMixin.java
+++ src/main/java/folk/sisby/drogstyle/mixin/PlayerEntityMixin.java
@@ -3,22 +3,22 @@ package folk.sisby.drogstyle.mixin;
 import com.unascribed.drogtor.DrogtorPlayer;
 import eu.pb4.stylednicknames.NicknameHolder;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = PlayerEntity.class, priority = 1001)
 public class PlayerEntityMixin implements DrogtorPlayer {
-	@Shadow
-	public ServerPlayNetworkHandler networkHandler;
+	private NicknameHolder getHolder() {
+		return (NicknameHolder) ((ServerPlayerEntity) (Object) this).networkHandler;
+	}
 
 	@Override
 	public void drogtor$setNickname(@Nullable String nickname) {
-		((NicknameHolder) networkHandler).sn_set(nickname, true);
+		getHolder().sn_set(nickname, true);
 	}
 
 	@Override
@@ -33,7 +33,7 @@ public class PlayerEntityMixin implements DrogtorPlayer {
 
 	@Override
 	public @Nullable String drogtor$getNickname() {
-		Text nickname = ((NicknameHolder) networkHandler).sn_getParsed();
+		Text nickname = getHolder().sn_getParsed();
 		if (nickname == null) return null;
 		return nickname.getString();
 	}
@@ -43,7 +43,7 @@ public class PlayerEntityMixin implements DrogtorPlayer {
 	 */
 	@Override
 	public @Nullable Formatting drogtor$getNameColor() {
-		Text nickname = ((NicknameHolder) networkHandler).sn_getParsed();
+		Text nickname = getHolder().sn_getParsed();
 		if (nickname == null) return null;
 		if (nickname.getStyle().getColor() == null) return null;
 		return Formatting.byName(nickname.getStyle().getColor().getName());
@@ -51,7 +51,7 @@ public class PlayerEntityMixin implements DrogtorPlayer {
 
 	@Override
 	public @Nullable String drogtor$getBio() {
-		Text nickname = ((NicknameHolder) networkHandler).sn_getParsed();
+		Text nickname = getHolder().sn_getParsed();
 		if (nickname == null) return null;
 		HoverEvent hoverEvent = nickname.getStyle().getHoverEvent();
 		if (hoverEvent == null) return null;
@@ -62,6 +62,6 @@ public class PlayerEntityMixin implements DrogtorPlayer {
 
 	@Override
 	public boolean drogtor$isActive() {
-		return ((NicknameHolder) networkHandler).sn_get() != null;
+		return getHolder().sn_get() != null;
 	}
 }
--- src/main/java/folk/sisby/drogstyle/mixin/styled_nicknames/CommandsMixin.java
+++ src/main/java/folk/sisby/drogstyle/mixin/styled_nicknames/CommandsMixin.java
@@ -5,6 +5,11 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = eu.pb4.stylednicknames.command.Commands.class, remap = false)
 public class CommandsMixin {
+	/**
+	 * @author Sisby folk
+	 * @reason Intended to completely removes styled nicknames command registration for drogstyle.
+	 * Allowing only info and /reload was too hard.
+	 */
 	@Overwrite
 	public static void register() {}
 }
