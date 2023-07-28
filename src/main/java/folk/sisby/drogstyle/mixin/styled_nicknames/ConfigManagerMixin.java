4355dfb5f34acb86c9b61406354bac99521d904b
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-07-28 17:25:31 +1000

    shorten mixin

--- src/main/java/folk/sisby/drogstyle/mixin/styled_nicknames/ConfigManagerMixin.java
+++ src/main/java/folk/sisby/drogstyle/mixin/styled_nicknames/ConfigManagerMixin.java
@@ -1,12 +1,13 @@
 package folk.sisby.drogstyle.mixin.styled_nicknames;
 
-import eu.pb4.stylednicknames.config.Config;
+import eu.pb4.placeholders.api.parsers.TextParserV1;
 import eu.pb4.stylednicknames.config.ConfigManager;
+import eu.pb4.stylednicknames.config.data.ConfigData;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
 
 /**
  * Modifies Styled Nicknames to allow nicknames to be self-assigned by any player, with colour and tooltips.
@@ -15,32 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(value = ConfigManager.class, remap = false)
 public abstract class ConfigManagerMixin {
-	@Shadow private static Config CONFIG;
-
-	@Inject(method = "getConfig", at = @At("RETURN"))
-	private static void forceAllowByDefault(CallbackInfoReturnable<Config> cir) {
-		if ("#${nickname}".equals(CONFIG.configData.nicknameFormat)) CONFIG.configData.nicknameFormat = "${nickname}"; // default to seamless nicknames
-		CONFIG.configData.allowByDefault = true;
-		CONFIG.configData.defaultEnabledFormatting.put("color", true);
-		CONFIG.configData.defaultEnabledFormatting.put("hover", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_red", true);
-		CONFIG.configData.defaultEnabledFormatting.put("yellow", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_blue", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_purple", true);
-		CONFIG.configData.defaultEnabledFormatting.put("gold", true);
-		CONFIG.configData.defaultEnabledFormatting.put("red", true);
-		CONFIG.configData.defaultEnabledFormatting.put("aqua", true);
-		CONFIG.configData.defaultEnabledFormatting.put("gray", true);
-		CONFIG.configData.defaultEnabledFormatting.put("light_purple", true);
-		CONFIG.configData.defaultEnabledFormatting.put("white", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_gray", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_grey", true);
-		CONFIG.configData.defaultEnabledFormatting.put("green", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_green", true);
-		CONFIG.configData.defaultEnabledFormatting.put("black", true);
-		CONFIG.configData.defaultEnabledFormatting.put("grey", true);
-		CONFIG.configData.defaultEnabledFormatting.put("orange", true);
-		CONFIG.configData.defaultEnabledFormatting.put("blue", true);
-		CONFIG.configData.defaultEnabledFormatting.put("dark_aqua", true);
+	@ModifyVariable(method = "loadConfig", at = @At(value = "INVOKE", target = "Ljava/io/BufferedWriter;<init>(Ljava/io/Writer;)V"))
+	private static ConfigData forceDefaultEnabledColorHover(ConfigData configData) {
+		configData.allowByDefault = true;
+		TextParserV1.SAFE.getTags().stream().filter(t -> List.of("color", "hover_event").contains(t.type())).forEach(tag -> configData.defaultEnabledFormatting.put(tag.name(), true));
+		return configData;
 	}
 }
